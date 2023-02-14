32a041f0060e1399fd20ceb27271ccf040f35e3f
Aluno <aluno@naverecife.local>
2023-02-14 19:51:14 -0300

    Initial commit

---
+++ src/AnimalMamifero.java
@@ -0,0 +1,30 @@
+
+public class AnimalMamifero {
+    private String tipoDePelo;
+    private String glandulasMamarias;
+
+    public AnimalMamifero() {
+        this.tipoDePelo = tipoDePelo;
+        this.glandulasMamarias = glandulasMamarias;
+    }
+
+    public void AnimalMamifero() {
+
+    }
+
+    public String getTipoDePelo() {
+        return tipoDePelo;
+    }
+
+    public void setTipoDePelo(String tipoDePelo) {
+        this.tipoDePelo = tipoDePelo;
+    }
+
+    public String getGlandulasMamarias() {
+        return glandulasMamarias;
+    }
+
+    public void setGlandulasMamarias(String glandulasMamarias) {
+        this.glandulasMamarias = glandulasMamarias;
+    }
+}
